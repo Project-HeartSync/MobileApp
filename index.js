10a2d62f285e1ed8b2ba9752c5d95a9d5988918b
jiahui0309 <e0970466@u.nus.edu>
2024-03-21 23:29:34 +0800

    Add backend user authentication

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+/*import {AppRegistry} from 'react-native';
 import App from './App';
 import Login from './App/Login';
 import Home from './App/Home';
@@ -12,4 +12,25 @@ import NotificationsScreen from './App/Notifications';
 //AppRegistry.registerComponent(appName, () => App);
 
 // open login screen
- AppRegistry.registerComponent(appName, () => App);
+ AppRegistry.registerComponent(appName, () => App);*/
+
+ import { AppRegistry } from 'react-native';
+import AppWrapper from './App/AppWrapper';
+import { name as appName } from './app.json';
+import { SYNC_CONFIG } from './sync.config';
+
+const RootComponent = () => {
+  const { enabled, appId } = SYNC_CONFIG;
+  print(appId);
+
+  if (enabled) {
+    // Render the synced version of the app
+    return <AppWrapper appId={appId} />;
+  } else {
+    // Render the non-synced version of the app
+    return <AppWrapper />;
+  }
+};
+
+AppRegistry.registerComponent(appName, () => RootComponent);
+
